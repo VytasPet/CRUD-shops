ee403acf946f7d98d48f8f6690295dcdae5dbddd
Vytautas P <v.petrucionis@gmail.com>
2023-04-27 13:00:45 +0300

    init - tailwind start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LoginPage from "./components/LoginPage";
+
+function App() {
+  return (
+    <div>
+      <h1 className="lg:text-5xl md:text-4xl sm:text-2xl text-xl text-center mt-10 underline">laba diena, shopas</h1>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,10 @@
+function LoginPage() {
+  return (
+    <div>
+      <h1 className="bg-tomato text-white py-20">Login Page</h1>
+      <p className="bg-black text-white mt-5 p-5">Cia bus kuriama eshop login</p>
+    </div>
+  );
+}
+
+export default LoginPage;
